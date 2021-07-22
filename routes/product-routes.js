01787f01e493e51ee778d66c668c8ea14e443ae3
hkarambizi <hkarambizi@gmail.com>
2021-07-22 15:47:47 -0400

    Swagger UI docs at api-port/api/v1/docs
    - localhost:5000/api/v1/docs will show API docs by Swagger

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,3 +1,60 @@
+/**
+ * @swagger
+ * /trending:
+ *   get:
+ *     summary: Retrieve a list of trending products.
+ *     description: Retrieve a list of trending products ordered on SnackPass Interview application.
+ *     parameters:
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *         description: The sorting mechanism for the list of products. Options include "popularity" and "recent"
+ *     responses:
+ *       200:
+ *         description: A list of products.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The auto-generated id of the book
+ *                 itemName:
+ *                   type: string
+ *                   description: The name of the products.
+ *                 restaurant:
+ *                   type: string
+ *                   description: The restaurant where the product was ordered.
+ *                 orderCount:
+ *                   type: number
+ *                   description: The count of orders of the particular product
+ *                 discountDecimal:
+ *                   type: number
+ *                   description: The decimal representation of the discounted price
+ *                 lastOrderedAt:
+ *                   type: string
+ *                   format: date
+ *                   description: The createdAt for the last order of this product.$ref
+ *                 createdAt:
+ *                   type: string
+ *                   format: date
+ *                   description: The date of the record creation.
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date
+ *                   description: The date of the record update.
+ *               example:
+ *                   itemName: Chocolate Pudding Cake
+ *                   restaurant: Mary Mac
+ *                   productPrice: 7.99
+ *                   discountDecimal: 0.92
+ *                   orderCount: 1
+ *                   lastOrderedAt: 2021-07-19T03:13:21.298Z
+ *                   createdAt: 2021-07-19T03:13:21.298Z
+ *                   updatedAt: 2021-07-19T03:13:21.298Z
+ */
 const express = require('express');
 const router = express.Router()
 // Controller
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// path is not defined
-const path =require('path');
-
-const express = require('express');
+const path = require('path');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  swaggerJsdoc = require('swagger-jsdoc'),
+  swaggerUi = require('swagger-ui-express');
 require('dotenv').config() // access environment variables
 const logger = require('./utils/logger')(__filename);
 const dbConnect = require('./db');
@@ -17,25 +18,53 @@ const apiVersionNamespace = `/api/${apiVersion}`;
 // import routes
 const productsRouter = require('./routes/product-routes');
 
+
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Snackpass Products API",
+      version: "0.1.0",
+      description: "This Express API documented by Swagger is used for the Trending Products App built for Harry Karambizi's Interview",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Trending Products App",
+        url: "https://harry-is-trending-at-snackpass.herokuapp.com/",
+        email: "hkarambizi@gmail.com"
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:5000${apiVersionNamespace}/products`
+      }
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
 // register routes
 app.use(`${apiVersionNamespace}/products`, productsRouter);
 
-// Route to see API docs
-app.get(`${apiVersionNamespace}/docs`, (req, res)=> {
-    res.send(`You have reached the SnackPass API ${apiVersion} documentation`)
-})
+// Route to see Swagger API docs
+app.use(`${apiVersionNamespace}/docs`, swaggerUi.serve, swaggerUi.setup(specs));
 
 // fpr deploying to Heroku
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // start server
-app.listen(port, ()=>{
-    logger.log('🍬 🍪 🌮  Snackpass API is live on port 5000!')
+app.listen(port, () => {
+  logger.log('🍬 🍪 🌮  Snackpass API is live on port 5000!')
 })
 
